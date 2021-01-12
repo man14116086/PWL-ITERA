212ad385f14ee9bc402db5567413d36c35544d03
man14116086 <firmanoloan@hotmail.com>
2021-01-12 18:25:45 +0700

    up

---
+++ _site/assets/js/main.js
@@ -0,0 +1,7 @@
+if(new Date().getHours()>18 || new Date().getHours()<6){
+    document.body.style.backgroundColor="Black";
+    document.body.style.color="White";
+}else{
+    document.body.style.backgroundColor="White"
+    document.body.style.color="Black"
+}
